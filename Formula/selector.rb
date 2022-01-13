0a5086abda27bbdc7b7e31a16f8d5c1d03b28571
Magnus Hirth <magnus.hirth@gmail.com>
2022-01-13 17:10:39 +0100

    Add selector formula

---
+++ Formula/selector.rb
@@ -0,0 +1,17 @@
+class Selector < Formula
+  desc "Terminal app for selecting between arguments and printing the result to stdout"
+  homepage "https://github.com/MHmorgan/selector"
+  url "https://github.com/MHmorgan/selector/archive/refs/tags/v1.2.0.tar.gz"
+  sha256 "46258395665da4adf8702ab9714a0a76b51a2fd04a3028d1a76079117f6e3166"
+  license "MPL-2.0"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    system "#{bin}/selector", "--version"
+  end
+end
