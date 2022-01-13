56abfc63db5f0d0e21b4bdb328b55046baa2c683
Magnus Hirth <magnus.hirth@gmail.com>
2022-01-13 17:39:26 +0100

    Don't version selector formula name

--- Formula/selector.rb
+++ Formula/selector.rb
@@ -1,4 +1,4 @@
-class SelectorAT12 < Formula
+class Selector < Formula
   desc "Terminal app for selecting between arguments and printing the result to stdout"
   homepage "https://github.com/MHmorgan/selector"
   url "https://github.com/MHmorgan/selector/archive/refs/tags/v1.2.0.tar.gz"
