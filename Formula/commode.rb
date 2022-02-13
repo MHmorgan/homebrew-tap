fe262fed1df1be52a3f0c4cb6409ae2e3699cbb1
Magnus Hirth <magnus.hirth@gmail.com>
2022-02-13 15:50:08 +0100

    Update to commode v2.0.0

--- Formula/commode.rb
+++ Formula/commode.rb
@@ -3,15 +3,40 @@ class Commode < Formula
 
   desc "Terminal client for Cabinet file server."
   homepage "https://github.com/MHmorgan/commode"
-  url "https://github.com/MHmorgan/commode/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "b2b0b74e1f0bc99db86b412456e0e78cf3c5c9ab67726b92b62e78539b12bf7e"
+  url "https://files.pythonhosted.org/packages/85/24/ed5995e4930065c3b1889bb6356cec735afc88079581af64f6e9df834649/commode-2.0.0.tar.gz"
+  sha256 "621c177412dd55353e73d687eab60f011f6c405b3f90ccd73a4e868279a750d6"
   license "MPL-2.0"
 
   depends_on "python@3.10"
 
+  resource "certifi" do
+    url "https://files.pythonhosted.org/packages/6c/ae/d26450834f0acc9e3d1f74508da6df1551ceab6c2ce0766a593362d6d57f/certifi-2021.10.8.tar.gz"
+    sha256 "78884e7c1d4b00ce3cea67b44566851c4343c120abd683433ce934a68ea58872"
+  end
+
+  resource "charset-normalizer" do
+    url "https://files.pythonhosted.org/packages/e8/e8/b6cfd28fb430b2ec9923ad0147025bf8bbdf304b1eb3039b69f1ce44ed6e/charset-normalizer-2.0.11.tar.gz"
+    sha256 "98398a9d69ee80548c762ba991a4728bfc3836768ed226b3945908d1a688371c"
+  end
+
   resource "click" do
-    url "https://files.pythonhosted.org/packages/4e/b4/a2279d9d0183784e9dd9fd62b7505adab0819662d4b169602a6fef6a5a72/click8-8.0.1.tar.gz"
-    sha256 "3412342408c03fbfd3b1826c8fd0fb2caace9b15a9d1c9375555122855449c86"
+    url "https://files.pythonhosted.org/packages/f4/09/ad003f1e3428017d1c3da4ccc9547591703ffea548626f47ec74509c5824/click-8.0.3.tar.gz"
+    sha256 "410e932b050f5eed773c4cda94de75971c89cdb3155a72a0831139a79e5ecb5b"
+  end
+
+  resource "idna" do
+    url "https://files.pythonhosted.org/packages/62/08/e3fc7c8161090f742f504f40b1bccbfc544d4a4e09eb774bf40aafce5436/idna-3.3.tar.gz"
+    sha256 "9d643ff0a55b762d5cdb124b8eaa99c66322e2157b69160bc32796e824360e6d"
+  end
+
+  resource "Jinja2" do
+    url "https://files.pythonhosted.org/packages/91/a5/429efc6246119e1e3fbf562c00187d04e83e54619249eb732bb423efa6c6/Jinja2-3.0.3.tar.gz"
+    sha256 "611bb273cd68f3b993fabdc4064fc858c5b47a973cb5aa7999ec1ba405c87cd7"
+  end
+
+  resource "MarkupSafe" do
+    url "https://files.pythonhosted.org/packages/bf/10/ff66fea6d1788c458663a84d88787bae15d45daa16f6b3ef33322a51fc7e/MarkupSafe-2.0.1.tar.gz"
+    sha256 "594c67807fb16238b30c44bdf74f36c02cdf22d1c8cda91ef8a0ed8dabf5620a"
   end
 
   resource "requests" do
@@ -24,26 +49,17 @@ class Commode < Formula
     sha256 "0e7c33d9a63e7ddfcb86780aac87befc2fbddf46c58dbb487e0855f7ceec283c"
   end
 
-  resource "idna" do
-    url "https://files.pythonhosted.org/packages/62/08/e3fc7c8161090f742f504f40b1bccbfc544d4a4e09eb774bf40aafce5436/idna-3.3.tar.gz"
-    sha256 "9d643ff0a55b762d5cdb124b8eaa99c66322e2157b69160bc32796e824360e6d"
+  resource "wheel" do
+    url "https://files.pythonhosted.org/packages/c0/6c/9f840c2e55b67b90745af06a540964b73589256cb10cc10057c87ac78fc2/wheel-0.37.1.tar.gz"
+    sha256 "e9a504e793efbca1b8e0e9cb979a249cf4a0a7b5b8c9e8b65a5e39d49529c1c4"
   end
-
-  resource "charset-normalizer" do
-    url "https://files.pythonhosted.org/packages/48/44/76b179e0d1afe6e6a91fd5661c284f60238987f3b42b676d141d01cd5b97/charset-normalizer-2.0.10.tar.gz"
-    sha256 "876d180e9d7432c5d1dfd4c5d26b72f099d503e8fcc0feb7532c9289be60fcbd"
-  end
-
-  resource "certifi" do
-    url "https://files.pythonhosted.org/packages/6c/ae/d26450834f0acc9e3d1f74508da6df1551ceab6c2ce0766a593362d6d57f/certifi-2021.10.8.tar.gz"
-    sha256 "78884e7c1d4b00ce3cea67b44566851c4343c120abd683433ce934a68ea58872"
-  end
-
 
   def install
+    virtualenv_create(libexec, "python@3.10")
     virtualenv_install_with_resources
   end
 
   test do
+    false
   end
 end
